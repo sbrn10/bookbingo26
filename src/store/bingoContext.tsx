0244ea4094430676eb1b40a069caf44613580e29
philat <26887746+sbrn10@users.noreply.github.com>
2026-08-15 16:21:18 +0900

    import static data

--- src/store/bingoContext.tsx
+++ src/store/bingoContext.tsx
@@ -1,13 +1,17 @@
-import {
-  createContext,
-  type Dispatch,
-  type SetStateAction,
-} from "react";
+import { createContext, type Dispatch, type SetStateAction } from "react";
+
+export type BookType = {
+  title: string;
+  author: string;
+  readStatus: number;
+  emoji: string[];
+};
 
 export type ContextType = {
   bingoCategories: string[];
   showModal: boolean;
   setShowModal: Dispatch<SetStateAction<boolean>>;
+  bookList: BookType[];
 };
 
 export const bingoContext = createContext<ContextType>({} as ContextType);
